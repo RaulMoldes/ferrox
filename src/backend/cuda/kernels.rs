85e283965efd32a6a295b058215cfbee07fec746
RaulMoldes <raulmoldes@gmail.com>
2025-06-18 15:03:27 +0200

    closure expected an argument, but none was provided.

--- src/backend/cuda/kernels.rs
+++ src/backend/cuda/kernels.rs
@@ -593,7 +593,7 @@ impl CudaKernels {
                 dummy = self
                     .device
                     .alloc_zeros::<i32>(input.len())
-                    .or_else(|| "Unable to allocate memory".to_string())?;
+                    .or_else(|_| "Unable to allocate memory".to_string())?;
                 &mut dummy.clone()
             }
         };
